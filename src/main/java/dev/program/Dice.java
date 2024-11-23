b640aa986978dea7151563256bee89feee14fa76
mallick1126 <rahulmallick1126@gmail.com>
2024-11-23 13:38:28 +0530

    added dice entity

---
+++ src/main/java/dev/program/Dice.java
@@ -0,0 +1,20 @@
+package dev.program;
+
+import java.util.Random;
+
+public class Dice {
+    private final int sides;
+    private final Random random;
+
+    public Dice(int sides){
+        if(sides<1){
+            throw  new IllegalArgumentException("Dice must have at least 1 side");
+        }
+        this.sides = sides;
+        this.random = new Random();
+    }
+
+    public int roll(){
+        return random.nextInt(sides);
+    }
+}
