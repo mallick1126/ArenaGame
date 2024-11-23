302d0a16fd069017b474b88b154351338f4b884b
mallick1126 <rahulmallick1126@gmail.com>
2024-11-23 15:15:24 +0530

    object creation using builder

--- src/main/java/dev/program/Main.java
+++ src/main/java/dev/program/Main.java
@@ -4,8 +4,18 @@ import dev.program.Player.Player;
 
 public class Main {
     public static void main(String[] args) {
-        Player playerA = new Player("Player A", 50, 5, 10);
-        Player playerB = new Player("Player B", 100, 10, 5);
+        Player playerA = new Player.Builder("Player A")
+                .health(150)
+                .strength(20)
+                .attack(15)
+                .build();
+
+        Player playerB = new Player.Builder("Player B")
+                .health(100)
+                .strength(10)
+                .attack(25)
+                .build();
+
 
         Arena arena = new Arena();
         Player winner = arena.fight(playerA, playerB);
